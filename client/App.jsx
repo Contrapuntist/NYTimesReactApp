43cb46ced8c781def5c00b9d584b8a39691e8443
Contrapuntist <contrapuntist@gmail.com>
2017-09-25 21:48:59 -0500

    got basic search component with sample bootstrap form to render

--- client/App.jsx
+++ client/App.jsx
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
-import Main from './components/Main'
-
+import Main from './components/Main.jsx';
+import Search from './components/Search.jsx';
 
 export default class App extends Component {
   render() {
     return (
-      <div style={{textAlign: 'center'}}>
-        <h1>Hello World</h1>
-      </div>);
-      <Main /> 
+      // <div style={{textAlign: 'center'}}>
+      //   <h1>Hello World</h1>
+
+      // </div>
+      <div>
+       <Main> 
+         <Search />
+       </Main>
+      </div>  
+    );
+
+
 
   }
 }
